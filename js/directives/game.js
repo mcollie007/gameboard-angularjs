d9e4d08ab952d53351c0b7640e4c0513f0ea1e6d
Michael Collie-Acasio <mcollie_fhc@hotmail.com>
2015-02-23 03:36:43 -0500

    GameBoard -Leve1 2 -Project 2
    http://s3.amazonaws.com/codecademy-content/projects/4/gameboard/README.h
    tml
    
    Using directives in angular js

---
+++ js/directives/game.js
@@ -0,0 +1,11 @@
+app.directive('game', function(){
+    return {
+        restrict: 'E',
+        scope: {
+            info: '='
+        },
+        templateUrl: 'js/directives/game.html'
+    
+    };
+
+});
